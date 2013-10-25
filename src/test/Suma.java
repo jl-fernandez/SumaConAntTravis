aee4cbdaf46c86cace6f154433378df2bcf8bd3a
jl-fernandez <fernandezfernandezjl@gmail.com>
2013-10-25 20:25:04 +0200

    commit inicio travis

---
+++ src/test/Suma.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Suma {
+	public double getSuma(double operando1, double operando2) {
+		return operando1 + operando2;
+	}
+
+	public double incrementa(double operando) {
+		return operando + 1;
+	}
+}
+
